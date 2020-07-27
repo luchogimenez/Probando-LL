4c3c6f4525fbd07b7c2722c9cd9ce67a93eca5f1
luchogimenez <gimenezluis001@gmail.com>
2020-07-27 20:02:32 -0300

    cambio de mensaje en parrafo

--- lyl/src/App.js
+++ lyl/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Bienvenido luis y leo
+         Bienvenidos muchachos
         </p>
         <a
           className="App-link"
