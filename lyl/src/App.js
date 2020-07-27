88ed39c29e749722a3f5f6dcf1c2f4caa7d75ad1
luchogimenez <gimenezluis001@gmail.com>
2020-07-27 20:17:14 -0300

    hola

--- lyl/src/App.js
+++ lyl/src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
          Bienvenidos muchachos
+         hola
         </p>
         <a
           className="App-link"
